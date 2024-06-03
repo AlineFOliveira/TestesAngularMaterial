b3246f9f25807570102867decbebdfdd6c02401e
Aline <aliferroli18@gmail.com>
2024-06-03 20:17:13 -0300

    03/06

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AutocompleteComponent } from './autocomplete/autocomplete.component';
 import { FormComponent } from './form/form.component';
-
+import { CardComponent } from './card/card.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AutocompleteComponent, FormComponent],
+  imports: [RouterOutlet, AutocompleteComponent, FormComponent, CardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/autocomplete/autocomplete.component.ts
+++ src/app/autocomplete/autocomplete.component.ts
@@ -26,17 +26,23 @@ export class AutocompleteComponent {
   pesquisar = new FormControl();
   cidades: string[] = ['Cascavel', 'Laranjeiras', 'Toledo', 'Curitiba', 'Foz do Iguaçu'];
 
-  filtro!: Observable<string[]>
+  filtro!: Observable<string[]>//emitirá arrays de string
 
   ngOnInit(){
-    this.filtro = this.pesquisar.valueChanges.pipe(
-      startWith(''),
-      map(value => this._filtro(value))
+    //pipes permitem transformar, filtrar e combinar os valores emitidos pelo observable
+    this.filtro = this.pesquisar.valueChanges.pipe(//se o valor do campo de pesquisa muda
+      startWith(''), //emite um valor inicial antes de emitir valores de mudança
+      map(value => this._filtro(value))//'mapeia'/pega cada valor emitido e passa para a função _filtro
     )
   }
 
   private _filtro(value: string): string[]{
     const filtroValue = value.toLowerCase(); //letras minusculas
+    //value tepresenta o texto digitado pelo usuário
     return this.cidades.filter(city => city.toLowerCase().includes(filtroValue));
+    //o filter cria um novo array...?
+    //city é cada elemento no array das cidades
+    //verifica se filtroValue está no nome da cidade do array.
+    // o includes retorna true se o que for passado for encontrado na string
   }
 }
